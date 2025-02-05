6658d3dcb7b2c74ea45bf16129fcf591865e851d
Jay <julius@dual.ph>
2025-02-05 17:57:31 +0800

    github

--- svelte.config.js
+++ svelte.config.js
@@ -11,10 +11,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			base: "/juliuspj.github.io/"
-		}),
-
+		adapter: adapter({}),
+		paths: {
+			base: "/juliuspj.github.io"
+		},
 		alias: {
 			$components: "src/lib/components",
 			$assets: "src/assets"
